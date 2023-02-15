848624e383d1917226edd9b726a0f780c31f28e5
otivos <vostowoko@gmail.com>
2023-02-15 14:58:01 +0300

    Created Login/Register route

--- app/app.py
+++ app/app.py
@@ -7,5 +7,8 @@ app = create_app()
 db.init_app(app)
 migrate = Migrate(app, db)
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
--- app/models.py
+++ app/models.py
@@ -1,12 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
+
+class School(db.Model):
+    __tablename__ = "schools"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    #area = db.Column(db.String, nullable=False)
 
 class Student(db.Model):
     __tablename__ = "students"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String, nullable=False)
-
-
-
+    id = db.Column(db.Integer, primary_key=True )
+    firstname = db.Column(db.String, nullable=False )
+    lastname = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    birthdate = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String, nullable=False)
+    #school_id = db.Column(db.ForeignKey("schools.id"))
+    #school = db.relationship('School', backref=db.backref('students', lazy=True))
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
